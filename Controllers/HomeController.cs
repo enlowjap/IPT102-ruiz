c214587629bce18a04f6c98343b20a0236e67d8f
enlowjap <145821911+enlowjap@users.noreply.github.com>
2023-09-23 14:14:09 +0800

    Added and Updates
    yeah i nah

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,9 +6,10 @@ public class HomeController : Controller
     {
         var studentInfoArray = new[]
         {
-            new StudentInfoModel { Name = "John Doe", StudentId = "S001", StudentAge = "20" },
-            new StudentInfoModel { Name = "Jane Smith", StudentId = "S002", StudentAge = "22" },
-            new StudentInfoModel { Name = "Mike Johnson", StudentId = "S003", StudentAge = "31" }
+            new StudentInfoModel { Name = "Japhet Ruiz", StudentId = "201991", StudentAge = "20", Address = "Quezon" },
+            new StudentInfoModel { Name = "Victor Magtanggol", StudentId = "265478", StudentAge = "22", Address = "Manila" },
+            new StudentInfoModel { Name = "Ivone Adarna", StudentId = "201456", StudentAge = "31", Address = "Pasig" },
+            new StudentInfoModel { Name = "Camus Taang", StudentId = "199120", StudentAge = "26", Address = "Cainta" }
         };
 
         return View(studentInfoArray);
--- Models/StudentInfoModel.cs
+++ Models/StudentInfoModel.cs
@@ -3,4 +3,5 @@ public class StudentInfoModel
     public string Name { get; set; }
     public string StudentId { get; set; }
     public string StudentAge { get; set; }
+    public string Address { get; set; }
 }
